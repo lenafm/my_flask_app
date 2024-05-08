a88a52c4b4e1f341d39bb06e1e9a4df2a9ec492b
Angelikabeloz <belozangelika@gmail.com>
2024-05-08 23:34:30 +0200

    my wed

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,13 +1,16 @@
 from flask import render_template, flash, redirect, url_for, request
 from flaskapp import app, db
-from flaskapp.models import BlogPost, IpView, Day
+from flaskapp.models import BlogPost, IpView, Day, UkData
 from flaskapp.forms import PostForm
 import datetime
-
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 import json
 import plotly
 import plotly.express as px
+import numpy as np
+
 
 
 # Route for the home page, which is where the blog posts will be shown
@@ -71,3 +74,61 @@ def before_request_func():
         db.session.add(ip_view)  # insert into the ip_view table
 
     db.session.commit()  # commit all the changes to the database
+
+
+@app.route('/student-labour-votes-scatter')
+def student_labour_votes_scatter():
+    data = UkData.query.with_entities(UkData.constituency_name, UkData.c11FulltimeStudent, UkData.LabVote19).all()
+    df = pd.DataFrame(data, columns=['Constituency', 'Full-time Student (%)', 'Labour Party Votes'])
+    fig = px.scatter(df, x='Full-time Student (%)', y='Labour Party Votes', title='Percentage of Full-time Students vs. Labour Party Votes in UK Constituencies', color='Labour Party Votes', color_continuous_scale='RdYlBu')
+    fig.update_xaxes(title='Percentage of Full-time Students', tickfont=dict(size=14), title_font=dict(size=18))
+    fig.update_yaxes(title='Labour Party Votes', tickfont=dict(size=14), title_font=dict(size=18))
+    fig.update_layout(
+        margin=dict(l=50, r=50, t=100, b=100),
+        autosize=False,
+        width=1500,
+        height=800,
+        plot_bgcolor='rgba(240, 240, 240, 0.9)',
+        paper_bgcolor='rgba(240, 240, 240, 0.9)',
+        font=dict(size=16),
+    )
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('student_labour_votes_scatter.html', title='Percentage of Full-time Students vs. Labour Party Votes', graphJSON=graphJSON)
+
+
+@app.route('/heatmap_voting_patterns/<country>')
+def heatmap_voting_patterns(country):
+
+    data = UkData.query.filter_by(country=country).all()
+
+    df = pd.DataFrame([{
+        'Constituency': d.constituency_name,
+        'Region': d.region,
+        'Turnout 2019': d.Turnout19,
+        'Conservative Votes': d.ConVote19,
+        'Labour Votes': d.LabVote19,
+        'Lib Dem Votes': d.LDVote19,
+        'SNP Votes': d.SNPVote19,
+        'Plaid Cymru Votes': d.PCVote19,
+        'UKIP Votes': d.UKIPVote19,
+        'Green Votes': d.GreenVote19,
+        'Brexit Votes': d.BrexitVote19,
+        'Population Density': d.c11PopulationDensity,
+        'Percentage Retired': d.c11Retired
+    } for d in data])
+
+    numeric_df = df.select_dtypes(include=[np.number])  # This filters out non-numeric columns
+    correlation_matrix = numeric_df.corr()
+    fig = px.imshow(correlation_matrix,
+                    labels=dict(x="Variable", y="Variable", color="Correlation"),
+                    x=correlation_matrix.columns,
+                    y=correlation_matrix.columns,
+                    title=f"Correlation Heatmap of Voting Patterns and Demographics in {country}")
+
+
+    fig.update_xaxes(side="bottom")
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('heatmap_voting_patterns.html',
+                           title=f'Correlation Heatmap of Voting Patterns and Demographics in {country}',
+                           graphJSON=graphJSON)
